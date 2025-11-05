08b2804835328ea1c7420096b1fc9e4e38e5b686
angelmech <angelmech999@gmail.com>
2025-11-05 02:57:06 +0100

    uebung 2 corrections

--- src/main/scala/uebung/Uebung2.scala
+++ src/main/scala/uebung/Uebung2.scala
@@ -4,92 +4,112 @@ import scala.annotation.tailrec
 import scala.util.Random
 
 class Uebung2 {
-    // Aufgabe 1:
-    // Funktion "or", die nur das erste Argument auswertet (Short-Circuit Evaluation)
-    def or(x: Boolean, y: => Boolean): Boolean = {
-      if (x)
-        true
-      else
-        y
-    }
+  // Aufgabe 1:
+  // Funktion "or", die nur das erste Argument auswertet (Short-Circuit Evaluation)
+  def or(x: Boolean, y: => Boolean): Boolean = {
+    if (x)
+      true
+    else
+      y
+  }
 
-    // ---------------------------------------------------------------------------
+  def ex: Boolean = throw new Error("Dies ist ein Fehler")
 
-    // Aufgabe 2:
-    // Korrigierte Version von myMethod
-    // Ursprünglicher Code hatte falsche if statements:
-    // beim ersten if muss ein else folgen, sonst wird es ignoriert
-    def myMethod(param: Int): String = {
-      if (param < 0)
-        "kleiner null"
-      else if (param > 0)
-        "größer null"
-      else
-        "null"
-    }
+  or(x = true, y = ex)
+  or(ex, y = true)
 
-    // ---------------------------------------------------------------------------
 
-    // Aufgabe 3:
-    def aufgabe3(): Int = {
-      val x = {
-        val offset = 1;
-        {
-          val x = 2
-          val offset = 10
-          x + offset
-        } +
-          {
-            val x = 5
-            x + offset
-          }
+  // ---------------------------------------------------------------------------
+
+  // Aufgabe 2:
+  // Korrigierte Version von myMethod
+  // ursprünglicher Code hatte falsche if statements:
+  // beim ersten if muss ein else folgen, sonst wird es ignoriert
+
+  //Es wird nicht das gewünschte Ergebnis zurückgeliefert, da das Erfüllen der ersten
+  //Bedingung nicht zum Beenden der Funktion führt. Es müsste entweder die return-Anweisung
+  //verwendet werden (imperativer Ansatz) oder ein else hinzugefügt werden (funktionaler
+  //Ansatz). Wird ein else hinzugefügt, wird aus der gesamten Funktion ein Block gemacht, der
+  //nur noch aus einer Anweisung besteht. Somit könnten auch die geschweiften Klammern
+  //entfallen.
+  def myMethod(param: Int): String =
+    if (param < 0) "kleiner null"
+    else if (param > 0) "größer null"
+    else "null"
+
+
+  // ---------------------------------------------------------------------------
+
+  // Aufgabe 3:
+  def aufgabe3(): Int = {
+    val x = {
+      val offset = 1;
+      {
+        val x = 2
+        val offset = 10
+        x + offset
+      } + {
+        val x = 5
+        x + offset
       }
+    }
+    x
+  }
+  // x = 18
+  // wenn semikolon weg, würde compiler x=val offset=1 lesen was quatsch wäre,
+  // mit semikolon liest compiler val x=..., dann aber val offset=1; <- semikolon = ende des ausdrucks
+  // also kompiliert er zuerst das, dann liest er für val x=... weiter
+
+  //Musterlösung:
+  //Die Sichtbarkeit von Variablen entspricht den in Programmiersprachen üblichen Regeln: Wird
+  //ein Block definiert, so können die Variablen überschrieben werden, was dazu führt, dass die
+  //äußeren Werte ihre Sichtbarkeit verlieren. Die im Block definierten Variablen verlieren ihre
+  //Gültigkeit, sobald der Block beendet ist. Werden diese Regeln angewendet, so ergibt sich ein
+  //Wert von 18 für die Variable x.
+  //In Scala können Semikolons weggelassen werden. In diesem Fall jedoch nicht, weil der
+  //Compiler dies für die Klasse Int als weiteren Parameter wertet. Dies liegt daran, dass es in
+  //Scala multiple Parameterlisten gibt, was einfach nur bedeutet, dass eine Funktion nicht nur
+  //eine Parameterliste haben kann. Zum Beispiel wie in der folgenden add-Funktion:
+  //def add(x:Int)(y:Int):Int= x+y
+
+
+  // ---------------------------------------------------------------------------
+
+  // Aufgabe 4: Rekursion + Abbruchbedingung + Rückgabe des letzten gültigen Werts
+  // Funktion squareUnder(x, max): quadriert x wiederholt, bis das Ergebnis > max ist
+  @tailrec
+  final def squareUnder(x: Double, max: Double): Double = {
+    val squared = x * x
+    if (squared > max)
       x
-    }
-    // x = 18
-    // wenn semikolon weg, würde compiler x=val offset=1 lesen was quatsch wäre,
-    // mit semikolon liest compiler val x=..., dann aber val offset=1; <- semikolon = ende des ausdrucks
-    // also kompiliert er zuerst das, dann liest er für val x=... weiter
+    else
+      squareUnder(squared, max) // tail recursive call
+  }
 
+  // ---------------------------------------------------------------------------
 
-    // ---------------------------------------------------------------------------
-
-    // Aufgabe 4: Rekursion + Abbruchbedingung + Rückgabe des letzten gültigen Werts
-    // Funktion squareUnder(x, max): quadriert x wiederholt, bis das Ergebnis > max ist
+  //Aufgabe 5:
+  // Schreiben Sie eine Funktion teiler(zahl:Int):Int, die den größten ganzzahligen
+  //Teiler der übergebenen Zahl– kleiner als die Zahl selbst – berechnet. Das heißt, wird eine
+  //Primzahl übergeben, so gibt die Funktion 1 zurück, ist es keine Primzahl den
+  //entsprechenden Teiler
+  def teiler(zahl: Int): Int = {
     @tailrec
-    final def squareUnder(x: Double, max: Double): Double = {
-      val squared = x * x
-      if (squared > max)
-        x
+    def loop(i: Int): Int =
+      if (i == 1)
+        1
+      else if (zahl % i == 0)
+        i
       else
-        squareUnder(squared, max) // tail recursive call
-    }
+        loop(i - 1)
 
-    // ---------------------------------------------------------------------------
-
-    //Aufgabe 5:
-    // Schreiben Sie eine Funktion teiler(zahl:Int):Int, die den größten ganzzahligen
-    //Teiler der übergebenen Zahl– kleiner als die Zahl selbst – berechnet. Das heißt, wird eine
-    //Primzahl übergeben, so gibt die Funktion 1 zurück, ist es keine Primzahl den
-    //entsprechenden Teiler
-    def teiler(zahl: Int): Int = {
-      @tailrec
-      def loop(i: Int): Int =
-        if (i == 1)
-          1
-        else
-          if (zahl % i == 0)
-            i
-          else
-            loop(i - 1)
-
-      loop(zahl - 1)
-    }
+    loop(zahl - 1)
+  }
 
   //Aufgabe 6: Schreiben Sie eine Funktion, quersumme mit der folgenden Signatur: def
   //quersumme(zahl:Int):Int. Sie soll die Quersumme der Zahl berechnen, die an die Funktion
   //übergeben wurde.
-  def quersumme(zahl:Int):Int = {
+  def quersumme(zahl: Int): Int = {
     //quersumme ist z.b.: zahl=3160 --> quersumme = 3+1+6+0 = 10
     //bei negativer zahl: -57 -> 5+7 = 12
     val absolutZahl = math.abs(zahl)
@@ -105,14 +125,17 @@ class Uebung2 {
   //Aufgabe 7: Fibonacci -> fibo(X) funktion, die für eine beliebige Zahl X, die Fibonacci-Zahl
   //berechnet.
   // fibo: 0, 1, 1, 2, 3, 5, 8, 13, ...
-  def fibo(x: Int):Int = {
-    if (x==0)
-      0
-    else if (x==1)
-      1
-    else fibo(x-1) + fibo(x-2)
+  def fibo(x: Int): Int = {
+    if (x == 0) 0
+    else if (x == 1) 1
+    else fibo(x - 1) + fibo(x - 2)
   }
 
+  def fibo2(x: Int): BigInt = x match {
+    case 0 => 0
+    case 1 => 1
+    case x => fibo2(x - 1) + fibo2(x - 2)
+  }
 
 
   // Aufgabe 8: Wandeln Sie die Funktion aus Aufgabe 2 so um, dass der Aufruf der Funktion
@@ -155,44 +178,21 @@ class Uebung2 {
   //ist das kleinste gemeinsame Vielfache (kgV) dieser Zahlen.
   // kgv(a,b) = a*b/ggT(a,b)    ,   ggT = gcd(greatest common divisor)
 
-  def kleinsteTeilbareZahl(x: Int): Long = {
-
-    def istDurchAlleTeilbar(zahl: Long): Boolean = {
-      @tailrec
-      def pruefeTeiler(teiler: Int): Boolean = {
-        if (teiler > x) true
-        else if (zahl % teiler != 0) false
-        else pruefeTeiler(teiler + 1)
-      }
-      pruefeTeiler(1)
+  def searchNumber(maxDiv: Int): Int = {
+    @tailrec
+    def searchNumber2(maxDiv: Int, z: Int): Int = {
+      if (checkDiv(maxDiv, z)) z
+      else searchNumber2(maxDiv, z + 1)
     }
 
     @tailrec
-    def findeKandidat(kandidat: Long): Long = {
-      if (istDurchAlleTeilbar(kandidat)) kandidat
-      else findeKandidat(kandidat + 1)
+    def checkDiv(maxnum: Int, number: Int): Boolean = maxnum match {
+      case 1 => true
+      case _ => if (number % maxnum == 0) checkDiv(maxnum - 1, number) else false
     }
-    findeKandidat(1)
+    searchNumber2(maxDiv,1)
   }
 
-  // side note: one liner für teilbar check
-  def istDurchAlleTeilbar2(x:Int,zahl:Int):Boolean={
-    (1 to zahl).forall(divisor => x % divisor == 0)
-  }
-
-  // effizientere version mit gcd und lcm:
-  def kleinsteTeilbareZahlEffizient(obergrenze: Int): Long = {
-    @tailrec
-    def ggt(a: Long, b: Long): Long =
-      if (b == 0) a
-      else ggt(b, a % b)
-
-    def kgv(a: Long, b: Long): Long =
-      (a * b) / ggt(a, b)
-
-    // Reduziere alle Zahlen von 1..obergrenze zu ihrem KGV
-    (1L to obergrenze.toLong).reduce(kgv)
-  }
 
   //----------------------------------------------------------------------
 
@@ -226,6 +226,32 @@ class Uebung2 {
     summe(2, 0L)
   }
 
+  //Musterlösung
+  def sumPrimes(max: Int): BigInt = {
+    // Check if a number is prime
+    def is_prim(X: Int): Boolean = {
+      @annotation.tailrec
+      def calcPrim(X: Int, i: Int, Max: Int): Boolean = {
+        if (i >= Max) true
+        else if (X % i == 0) false
+        else calcPrim(X, i + 1, Max)
+      }
+      calcPrim(X, 2, math.sqrt(X).toInt + 1)
+    }
+
+    // Recursively sum all primes up to 'max'
+    @annotation.tailrec
+    def helperPrim(z: Int, sum: BigInt): BigInt = z match {
+      case 2 => sum + 2
+      case _ =>
+        if (is_prim(z)) helperPrim(z - 1, sum + z)
+        else helperPrim(z - 1, sum)
+    }
+
+    helperPrim(max, 0)
+  }
+
+
   //----------------------------------------------------------------------
 
   //Aufgabe 11: Implementieren Sie die Methode calculatePi, die auf Basis Zufall die Zahl Pi
@@ -255,17 +281,5 @@ class Uebung2 {
     }
     4.0 * (loop(n, 0).toDouble / n)
   }
-
-
-
-
-
-
-
-
-
-  // zum testen der short-circuit evaluation
-  def explode(): Boolean = throw new RuntimeException("Boom!")
-
 
 }
